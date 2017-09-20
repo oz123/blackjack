8cf3a649ac1f178f38ea467625d2ff0a9ed9e318
Oz N Tiram <oz.tiram@mobilityhouse.com>
2017-09-20 11:07:37 +0200

    Validate suite of a card
     Since we are creating a blackjack we validate that cards
     have a proper suit.

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -2,8 +2,14 @@
 class Card:
 
     def __init__(self, suit):
+
+        if suit not in ['spade', 'club', 'diamond', 'heart']:
+            raise ValueError(
+                "{} must be in ['spade', 'club', 'diamond', 'heart']")
+
         self.suit = suit
 
 
 class Deck:
+
     pass
--- tests/test_blackjack.py
+++ tests/test_blackjack.py
@@ -1,6 +1,18 @@
+import pytest
+
 from blackjack import Card, Deck
 
 
 def test_card_has_suite():
     card = Card('diamond')
     assert hasattr(card, 'suit')
+
+
+def test_card_has_valid_suite():
+
+    with pytest.raises(ValueError):
+        Card('robotricks')
+
+    c = Card('spade')
+
+    assert c.suit == 'spade'
