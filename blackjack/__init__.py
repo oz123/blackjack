059617431be3992857451eacbcefe5cee89753cc
Oz N Tiram <oz.tiram@mobilityhouse.com>
2017-09-21 10:18:22 +0200

    Add Numerical or Face values
      for each card instance we can create a value

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -1,13 +1,18 @@
 
 class Card:
 
-    def __init__(self, suit):
+    def __init__(self, suit, value):
 
         if suit not in ['spade', 'club', 'diamond', 'heart']:
             raise ValueError(
                 "{} must be in ['spade', 'club', 'diamond', 'heart']")
 
         self.suit = suit
+
+        if value not in ['A', 'Q', 'K', 'J'] and value not in list(range(2, 11)):
+            raise ValueError
+
+        self.value = value
 
 
 class Deck:
--- tests/test_blackjack.py
+++ tests/test_blackjack.py
@@ -4,15 +4,23 @@ from blackjack import Card, Deck
 
 
 def test_card_has_suite():
-    card = Card('diamond')
+    card = Card('diamond', 2)
     assert hasattr(card, 'suit')
 
 
 def test_card_has_valid_suite():
 
     with pytest.raises(ValueError):
-        Card('robotricks')
+        Card('robotricks', 2)
 
-    c = Card('spade')
+    c = Card('spade', 2)
 
     assert c.suit == 'spade'
+
+
+def test_card_has_a_value():
+    king_of_spades = Card('spade', 'K')
+    queen_of_spades = Card('spade', 'Q')
+
+    assert king_of_spades.value == 'K'
+    assert king_of_spades.value != 'Q'
