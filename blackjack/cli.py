4a6cfb6defd4bac9a4503a3001c9a53f89367953
Oz N Tiram <oz.tiram@mobilityhouse.com>
2017-10-12 11:35:25 +0200

    Add "ping-pong" server client
     * We now have a basic structure to build uppon
       into this skeleton we will later add the game logic

--- blackjack/cli.py
+++ blackjack/cli.py
@@ -1,2 +1,33 @@
+import argparse
+
+
+from blackjack.server import BlackjackServer
+from blackjack.client import BlackjackClient
+
+
+def get_parser():  # pragma: no coverage
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="cmd")
+
+    server = subparsers.add_parser("serve", description="Start a blackjack game server")
+    client = subparsers.add_parser("join", description="Join a Blackjack server")
+
+    client.add_argument("server", type=str, help="The server to join")
+
+    return parser
+
+
 def main():
-    print("Skeleton for our program")
+    parser = get_parser()
+    options = parser.parse_args()
+    if not options.cmd:
+        parser.print_help()
+
+    if options.cmd == 'serve':
+        with BlackjackServer() as server:
+            server.serve_forever()
+
+    if options.cmd == 'join':
+        BlackjackClient(options.server).run()
+
+
---
+++ blackjack/client.py
@@ -0,0 +1,26 @@
+import socket
+
+
+class BlackjackClient:
+
+    def __init__(self, address):
+        self.address = address
+
+    def run(self):
+
+        HOST, PORT = self.address.split(":")
+        PORT = int(PORT)
+        while True:
+            data = input("Enter data to send: ")
+            # Create a socket (SOCK_STREAM means a TCP socket)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                # Connect to server and send data
+                sock.connect((HOST, PORT))
+                sock.sendall(bytes(data + "\n", "utf-8"))
+
+                # Receive data from the server and shut down
+                received = str(sock.recv(1024), "utf-8")
+
+            print("Sent:     {}".format(data))
+            print("Received: {}".format(received))
+
